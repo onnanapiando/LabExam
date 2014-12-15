f7f76d8b620d82657b145e0431d0700327537930
onnanapiando <onnapiando@gmail.com>
2014-12-15 20:43:33 +0800

    unfinished assertEqual

--- test/unit/scorer_test.py
+++ test/unit/scorer_test.py
@@ -1,4 +1,7 @@
 import unittest
+from scorer.scorer import *
 
 class ScorerTest(unittest.TestCase):
-    
+    def test_score(self):
+        scorer = Scorer()
+        if self.assertEqual(score_x, )	
